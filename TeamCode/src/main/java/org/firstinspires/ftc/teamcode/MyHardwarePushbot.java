1018487faf435ebccd0c5c04f0196a5f3c1b5b44
Scott Padilla <linden.engineering.notebook@gmail.com>
2018-08-23 21:16:59 -0400

    First commit of teamcode files.  Uses helper class to define robot hardware and separate auton class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyHardwarePushbot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyHardwarePushbot.java
@@ -37,6 +37,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 /**
  * This is NOT an opmode.
  * Version 0.1.0
+ * Only 1 motor in this version
  * This class can be used to define all the specific hardware for a single robot.
  * In this case that robot is a Pushbot.
  * See PushbotTeleopTank_Iterative and others classes starting with "Pushbot" for usage examples.
